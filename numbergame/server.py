f3bb175c91dd4fe2da794b98dd5ea6e240a69cf6
Bald Mike <baldmike312@gmail.com>
2017-07-08 13:58:24 -0500

    updated numbergame

--- numbergame/server.py
+++ numbergame/server.py
@@ -1,30 +1,36 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, jsonify
 import random
 
 app = Flask (__name__)
 app.secret_key = "c2a1dfa3a243fa4e2eb035427f7af562" 
 
-randnum = random.randrange(1,101)
-
 @app.route('/')
 def index():
-    print randnum
-
+    if 'randnum' not in session:
+        session['randnum'] = random.randrange(1,101)
+    print str(session['randnum']) + " Is the number"
     return render_template('index.html')
 
-@app.route('/guess', methods=['POST'])
-def guess():
+@app.route('/process', methods=['POST'])
+def process():
     session['guess'] = int(request.form['guess'])
-    
-    if session['guess']==randnum:
+   
+    if session['guess'] < session['randnum']:
+        print str(session['guess']) + " is TOO LOW"
+        return render_template('guess.html', guessed_num = str(session['guess']) + ' is Too Low!')
+
+    elif session['guess'] > session['randnum']:
+        print str(session['guess']) + " is TOO HIGH"
+        return render_template('guess.html', guessed_num = str(session['guess']) + ' is Too High!')
+
+    elif session['guess'] == session['randnum']:
         print "YOU GUESSED IT"
+        answer = str(session['guess']) + " is right!"
+        return render_template('winner.html', guessed_num = str(session['guess']) + ' IS RIGHT!')
 
-    else:
-        print "you made it to guess"
-        print session['guess']
-    
+@app.route('/winner')
+def winner ():
+    session.clear()
     return redirect('/')
-
-
 
 app.run(debug=True)
--- numbergame/static/number.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-console.log('NOW READY')
- 
-// $('#submit').click(function() {
-//     window.location.assign('/guess')
-// })
-
-});
